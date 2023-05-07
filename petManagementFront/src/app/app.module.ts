182b7418c747b6ca197816f83a164e60ea459e85
f-u-g-g3r <102284163+f-u-g-g3r@users.noreply.github.com>
2023-05-07 20:11:23 +0300

    Add files via upload

---
+++ petManagementFront/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { PetService } from './services/pet.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ShowPetComponent } from './pages/showPets/showPets.component';
+import { AddPetComponent } from './pages/addPet/addPet.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './pages/login/login.component';
+import { UserService } from './services/user.service';
+
+const routes: Routes = [
+  { path: 'show-pets', component: ShowPetComponent },
+  { path: 'add-pet', component: AddPetComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: LoginComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShowPetComponent,
+    AddPetComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    RouterModule.forRoot(routes),
+    
+  ],
+  providers: [PetService, UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ petManagementFront/src/app/pages/addPet/addPet.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PetService } from '../../services/pet.service';
+import { PetTypes } from 'src/app/interfaces/petTypes';
+import { Country } from 'src/app/interfaces/countries';
+import { FurColors } from 'src/app/interfaces/furColors';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'addPet-root',
+    templateUrl: './addPet.html',
+    styleUrls: ['../design.css']
+  })
+
+export class AddPetComponent implements OnInit {
+  public petTypes: PetTypes[] = [];
+  public furColors: FurColors[] = [];
+  public countries: Country[] = [];
+  public uid!: number;
+  public userIdInput: any;
+  public username!: string;
+
+  constructor(private petService: PetService, private router: Router) {}
+  
+  ngOnInit() {
+    this.checkLogin();
+    this.getPetInfo();
+  }
+
+  public getPetInfo(): void {
+    this.petService.getPetTypes().subscribe({
+      next: (response: PetTypes[]) => this.petTypes = response,
+      error: console.error
+    });
+
+    this.petService.getFurColors().subscribe({
+      next: (response: FurColors[]) => this.furColors = response,
+      error: console.error
+    });
+
+    this.petService.getCountries().subscribe({
+      next: (response: Country[]) => this.countries = response,
+      error: console.error
+    });
+  }
+
+  public onAddPet(addForm: NgForm): void {
+    const formFields = addForm.value;
+    if(addForm.valid){
+      this.petService.addPet(formFields).subscribe({
+        complete: () => window.location.reload(),
+        error: console.error
+      });
+    }
+  }
+
+  public redirectToTablePage(): void {
+    this.router.navigate(['/show-pets']);
+  }
+
+  private checkLogin(): void {
+    this.uid = parseInt(sessionStorage.getItem('uid') || '');
+    if(Number.isNaN(this.uid)) {
+      this.router.navigate(['/login']);
+    }
+    this.username=sessionStorage.getItem('username') || '';
+  }
+
+  public signOut(): void {
+    sessionStorage.removeItem('uid');
+    sessionStorage.removeItem('username');
+    window.location.reload();
+  }
+}
